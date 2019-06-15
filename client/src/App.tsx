8061350bb3398f964b5617c943ff991be1543c80
Dani Tacheny <dani.tacheny@gmail.com>
2019-06-15 15:53:46 -0700

    initialize react app

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      App goes here
+    </div>
+  );
+}
+
+export default App;
